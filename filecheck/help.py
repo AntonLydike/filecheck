2ecdcd5d42133c6b11e174361a91552900701387
Anton Lydike <me@antonlydike.de>
2024-09-03 22:57:39 +0100

    fix typo in help text

--- filecheck/help.py
+++ filecheck/help.py
@@ -12,7 +12,7 @@ FLAGS:
                                   match.
 --match-full-lines              : Expect every check line to match the whole line.
 --reject-empty-vars             : Raise an error when a value captures an empty string.
---dump-intput                   : Dump the input to stderr annotated with helpful
+--dump-input                    : Dump the input to stderr annotated with helpful
                                   information depending on the context. Allowed values
                                   are help, always, never, fail. Default is fail.
                                   Only fail and never is currently supported in this
